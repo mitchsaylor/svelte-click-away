fe4f4e41c80a4dd22fbce5917029adb909d27f48
mitchsaylor <mitch@saylor.io>
2026-08-24 10:13:51 -0400

    docs: add typescript definitions

--- src/lib/clickaway.ts
+++ src/lib/clickaway.ts
@@ -1,5 +1,10 @@
 import type { Attachment } from 'svelte/attachments';
 
+/**
+ * A helper function for handling clicking away from a DOM node
+ * 
+ * @param node - Attach the DOM node using Svelte's {@attach clickaway}, then use the helper function with onclickaway={}.
+ */
 export const clickaway: Attachment = (node: Element) => {
 	const handleClick = (event: MouseEvent) => {
 		const target = event.target as Node | null;
